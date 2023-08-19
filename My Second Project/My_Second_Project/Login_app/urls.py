c541a3a24f95293baa4d4fb3340538803db0bda8
unknown <mdalamin123cpt@outlook.com>
2023-08-19 07:40:22 -0700

    Project Upload

--- My Second Project/My_Second_Project/Login_app/urls.py
+++ My Second Project/My_Second_Project/Login_app/urls.py
@@ -8,11 +8,11 @@ from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 app_name = 'Login_app'
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('register/', views.register, name='register'),
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
     path('login/', views.login_page, name='login'),
     path('user_login', views.user_login, name='user_login'),
-    # path('user_logout', views.user_logout, name='logout'),
+    path('user_logout', views.user_logout, name='logout'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- My Second Project/My_Second_Project/Login_app/views.py
+++ My Second Project/My_Second_Project/Login_app/views.py
@@ -33,47 +33,47 @@ def user_login(request):
         return HttpResponseRedirect(reverse('Login_app:login'))
             
         
-# @login_required
-# def user_logout(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('Login_app:index'))
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('Login_app:index'))
 
 
-# def index(request):
-#     dict = {}
-#     if request.user.is_authenticated:
-#         current_user = request.user
-#         user_id = current_user.id
-#         user_basic_info =  User.objects.get(pk = user_id)  
-#         user_more_info =  UserInfo.objects.get(user__pk= user_id) 
-#         dict = {'user_basic_info': user_basic_info, 'user_more_info': user_more_info}
-#     return render(request, 'Login_app/index.html', context=dict)
+def index(request):
+    dict = {}
+    if request.user.is_authenticated:
+        current_user = request.user
+        user_id = current_user.id
+        user_basic_info =  User.objects.get(pk = user_id)  
+        user_more_info =  UserInfo.objects.get(user__pk= user_id) 
+        dict = {'user_basic_info': user_basic_info, 'user_more_info': user_more_info}
+    return render(request, 'Login_app/index.html', context=dict)
 
-# def register(request):
+def register(request):
     
-#     registered = False
+    registered = False
     
-#     if request.method == "POST":
-#         user_form = UserForm(data=request.POST)
-#         user_info_form = UserInfoForm(data=request.POST)
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        user_info_form = UserInfoForm(data=request.POST)
         
-#         if user_form.is_valid() and user_info_form.is_valid():
-#             user = user_form.save(commit=True)
-#             user.set_password(user.password)
-#             user.save()
+        if user_form.is_valid() and user_info_form.is_valid():
+            user = user_form.save(commit=True)
+            user.set_password(user.password)
+            user.save()
             
-#             user_info = user_info_form.save(commit=False)
-#             user_info.user = user
+            user_info = user_info_form.save(commit=False)
+            user_info.user = user
             
-#             if 'profile_pic' in request.FILES:
-#                 user_info.profile_pic = request.FILES['profile_pic']
+            if 'profile_pic' in request.FILES:
+                user_info.profile_pic = request.FILES['profile_pic']
             
-#             user_info.save()
-#             registered = True
+            user_info.save()
+            registered = True
         
-#     else:
-#         user_form = UserForm()
-#         user_info_form = UserInfoForm()
+    else:
+        user_form = UserForm()
+        user_info_form = UserInfoForm()
         
-#     dict = {'user_form':user_form, 'user_info_form': user_info_form, 'registered': registered}
-#     return render(request, 'Login_app/register.html', context=dict)
+    dict = {'user_form':user_form, 'user_info_form': user_info_form, 'registered': registered}
+    return render(request, 'Login_app/register.html', context=dict)
